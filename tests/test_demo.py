a4a36192d85adee4b28099abafb31c208a34b157
Jin-Sun-tts <jin.sun@gsa.gov>
2023-04-13 15:12:55 -0400

    fix project name and test coverage

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,5 +1,5 @@
-from demo import __version__
-from demo.example import hello
+from datagovharvester import __version__
+from datagovharvester.example import hello
 
 def test_version():
     assert __version__ == "0.1.0"
