bc5e0cc7b0888128e3a279e4f72e3a64d43d0a95
Reid Hewitt <sam.hew.r@gmail.com>
2023-04-17 14:18:19 -0600

    add schema fixtures.

---
+++ tests/fixtures/data.py
@@ -0,0 +1,21 @@
+import pytest
+import json
+from pathlib import Path
+from utils.json_utilities import open_json
+
+BASE_DIR = Path(__file__).parents[2]
+SCHEMA_DIR = BASE_DIR / "schemas"
+
+
+@pytest.fixture
+def get_dataset_schema():
+
+    dataset_schema = SCHEMA_DIR / "dataset.json"
+    return open_json(dataset_schema)
+
+
+@pytest.fixture
+def get_catalog_schema():
+
+    catalog_schema = SCHEMA_DIR / "catalog.json"
+    return open_json(catalog_schema)
