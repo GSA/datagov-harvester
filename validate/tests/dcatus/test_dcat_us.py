1985c5fab2170a95f96273755dcf22468de37829
Reid Hewitt <sam.hew.r@gmail.com>
2023-04-19 14:47:45 -0600

    reoganize and format.

--- validate/tests/dcatus/test_dcat_us.py
+++ validate/tests/dcatus/test_dcat_us.py
@@ -2,7 +2,6 @@ import jsonschema
 
 
 def test_numerical_title(open_catalog_schema, open_numerical_title_json):
-
     dataset_schema = open_catalog_schema
     json_data = open_numerical_title_json
 
@@ -15,8 +14,9 @@ def test_numerical_title(open_catalog_schema, open_numerical_title_json):
         assert True
 
 
-def test_collection_1_parent_2_children(open_catalog_schema, open_collection_1_parent_2_children_json):
-
+def test_collection_1_parent_2_children(
+    open_catalog_schema, open_collection_1_parent_2_children_json
+):
     dataset_schema = open_catalog_schema
     json_data = open_collection_1_parent_2_children_json
 
@@ -30,7 +30,6 @@ def test_collection_1_parent_2_children(open_catalog_schema, open_collection_1_p
 
 
 def test_missing_catalog(open_catalog_schema, open_missing_catalog_json):
-
     dataset_schema = open_catalog_schema
     json_data = open_missing_catalog_json
 
@@ -44,7 +43,6 @@ def test_missing_catalog(open_catalog_schema, open_missing_catalog_json):
 
 
 def test_ny(open_catalog_schema, open_ny_json):
-
     dataset_schema = open_catalog_schema
     json_data = open_ny_json
 
@@ -57,8 +55,9 @@ def test_ny(open_catalog_schema, open_ny_json):
         assert True
 
 
-def test_missing_identifier_title(open_catalog_schema, open_missing_identifier_title_json):
-
+def test_missing_identifier_title(
+    open_catalog_schema, open_missing_identifier_title_json
+):
     dataset_schema = open_catalog_schema
     json_data = open_missing_identifier_title_json
 
@@ -72,7 +71,6 @@ def test_missing_identifier_title(open_catalog_schema, open_missing_identifier_t
 
 
 def test_usda_gov(open_catalog_schema, open_usda_gov_json):
-
     dataset_schema = open_catalog_schema
     json_data = open_usda_gov_json
 
@@ -86,7 +84,6 @@ def test_usda_gov(open_catalog_schema, open_usda_gov_json):
 
 
 def test_arm(open_catalog_schema, open_arm_json):
-
     dataset_schema = open_catalog_schema
     json_data = open_arm_json
 
@@ -100,7 +97,6 @@ def test_arm(open_catalog_schema, open_arm_json):
 
 
 def test_large_spatial(open_catalog_schema, open_large_spatial_json):
-
     dataset_schema = open_catalog_schema
     json_data = open_large_spatial_json
 
@@ -114,7 +110,6 @@ def test_large_spatial(open_catalog_schema, open_large_spatial_json):
 
 
 def test_reserved_title(open_catalog_schema, open_reserved_title_json):
-
     dataset_schema = open_catalog_schema
     json_data = open_reserved_title_json
 
@@ -127,8 +122,9 @@ def test_reserved_title(open_catalog_schema, open_reserved_title_json):
         assert False
 
 
-def test_collection_2_parent_4_children(open_catalog_schema, open_collection_2_parent_4_children_json):
-
+def test_collection_2_parent_4_children(
+    open_catalog_schema, open_collection_2_parent_4_children_json
+):
     dataset_schema = open_catalog_schema
     json_data = open_collection_2_parent_4_children_json
 
@@ -142,7 +138,6 @@ def test_collection_2_parent_4_children(open_catalog_schema, open_collection_2_p
 
 
 def test_geospatial(open_catalog_schema, open_geospatial_json):
-
     dataset_schema = open_catalog_schema
     json_data = open_geospatial_json
 
@@ -156,7 +151,6 @@ def test_geospatial(open_catalog_schema, open_geospatial_json):
 
 
 def test_null_spatial(open_catalog_schema, open_null_spatial_json):
-
     dataset_schema = open_catalog_schema
     json_data = open_null_spatial_json
 
