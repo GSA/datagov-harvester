199aeaa9edf677d3c3a46a569655af400c9d182d
robert-bryson <robert.bryson@gsa.gov>
2023-04-20 11:34:21 -0500

    validate_json_schema function to utils, refactored implementation

--- tests/validate/dcatus/test_dcat_us.py
+++ tests/validate/dcatus/test_dcat_us.py
@@ -1,17 +1,11 @@
-import jsonschema
+from utils.json_utilities import validate_json_schema
 
 
 def test_numerical_title(open_catalog_schema, open_numerical_title_json):
     dataset_schema = open_catalog_schema
     json_data = open_numerical_title_json
 
-    try:
-        jsonschema.validate(json_data, schema=dataset_schema)
-        assert False
-    except jsonschema.ValidationError as e:
-        error_message = "error: " + e.message + ". offending element: " + e.json_path
-        print(error_message)
-        assert True
+    validate_json_schema(json_data, dataset_schema)
 
 
 def test_collection_1_parent_2_children(
@@ -20,39 +14,21 @@ def test_collection_1_parent_2_children(
     dataset_schema = open_catalog_schema
     json_data = open_collection_1_parent_2_children_json
 
-    try:
-        jsonschema.validate(json_data, schema=dataset_schema)
-        assert True
-    except jsonschema.ValidationError as e:
-        error_message = "error: " + e.message + ". offending element: " + e.json_path
-        print(error_message)
-        assert False
+    validate_json_schema(json_data, dataset_schema)
 
 
 def test_missing_catalog(open_catalog_schema, open_missing_catalog_json):
     dataset_schema = open_catalog_schema
     json_data = open_missing_catalog_json
 
-    try:
-        jsonschema.validate(json_data, schema=dataset_schema)
-        assert False
-    except jsonschema.ValidationError as e:
-        error_message = "error: " + e.message + ". offending element: " + e.json_path
-        print(error_message)
-        assert True
+    validate_json_schema(json_data, dataset_schema)
 
 
 def test_ny(open_catalog_schema, open_ny_json):
     dataset_schema = open_catalog_schema
     json_data = open_ny_json
 
-    try:
-        jsonschema.validate(json_data, schema=dataset_schema)
-        assert False
-    except jsonschema.ValidationError as e:
-        error_message = "error: " + e.message + ". offending element: " + e.json_path
-        print(error_message)
-        assert True
+    validate_json_schema(json_data, dataset_schema)
 
 
 def test_missing_identifier_title(
@@ -61,65 +37,35 @@ def test_missing_identifier_title(
     dataset_schema = open_catalog_schema
     json_data = open_missing_identifier_title_json
 
-    try:
-        jsonschema.validate(json_data, schema=dataset_schema)
-        assert False
-    except jsonschema.ValidationError as e:
-        error_message = "error: " + e.message + ". offending element: " + e.json_path
-        print(error_message)
-        assert True
+    validate_json_schema(json_data, dataset_schema)
 
 
 def test_usda_gov(open_catalog_schema, open_usda_gov_json):
     dataset_schema = open_catalog_schema
     json_data = open_usda_gov_json
 
-    try:
-        jsonschema.validate(json_data, schema=dataset_schema)
-        assert True
-    except jsonschema.ValidationError as e:
-        error_message = "error: " + e.message + ". offending element: " + e.json_path
-        print(error_message)
-        assert False
+    validate_json_schema(json_data, dataset_schema)
 
 
 def test_arm(open_catalog_schema, open_arm_json):
     dataset_schema = open_catalog_schema
     json_data = open_arm_json
 
-    try:
-        jsonschema.validate(json_data, schema=dataset_schema)
-        assert True
-    except jsonschema.ValidationError as e:
-        error_message = "error: " + e.message + ". offending element: " + e.json_path
-        print(error_message)
-        assert False
+    validate_json_schema(json_data, dataset_schema)
 
 
 def test_large_spatial(open_catalog_schema, open_large_spatial_json):
     dataset_schema = open_catalog_schema
     json_data = open_large_spatial_json
 
-    try:
-        jsonschema.validate(json_data, schema=dataset_schema)
-        assert True
-    except jsonschema.ValidationError as e:
-        error_message = "error: " + e.message + ". offending element: " + e.json_path
-        print(error_message)
-        assert False
+    validate_json_schema(json_data, dataset_schema)
 
 
 def test_reserved_title(open_catalog_schema, open_reserved_title_json):
     dataset_schema = open_catalog_schema
     json_data = open_reserved_title_json
 
-    try:
-        jsonschema.validate(json_data, schema=dataset_schema)
-        assert True
-    except jsonschema.ValidationError as e:
-        error_message = "error: " + e.message + ". offending element: " + e.json_path
-        print(error_message)
-        assert False
+    validate_json_schema(json_data, dataset_schema)
 
 
 def test_collection_2_parent_4_children(
@@ -128,36 +74,18 @@ def test_collection_2_parent_4_children(
     dataset_schema = open_catalog_schema
     json_data = open_collection_2_parent_4_children_json
 
-    try:
-        jsonschema.validate(json_data, schema=dataset_schema)
-        assert True
-    except jsonschema.ValidationError as e:
-        error_message = "error: " + e.message + ". offending element: " + e.json_path
-        print(error_message)
-        assert False
+    validate_json_schema(json_data, dataset_schema)
 
 
 def test_geospatial(open_catalog_schema, open_geospatial_json):
     dataset_schema = open_catalog_schema
     json_data = open_geospatial_json
 
-    try:
-        jsonschema.validate(json_data, schema=dataset_schema)
-        assert True
-    except jsonschema.ValidationError as e:
-        error_message = "error: " + e.message + ". offending element: " + e.json_path
-        print(error_message)
-        assert False
+    validate_json_schema(json_data, dataset_schema)
 
 
 def test_null_spatial(open_catalog_schema, open_null_spatial_json):
     dataset_schema = open_catalog_schema
     json_data = open_null_spatial_json
 
-    try:
-        jsonschema.validate(json_data, schema=dataset_schema)
-        assert True
-    except jsonschema.ValidationError as e:
-        error_message = "error: " + e.message + ". offending element: " + e.json_path
-        print(error_message)
-        assert False
+    validate_json_schema(json_data, dataset_schema)
--- utils/json_utilities.py
+++ utils/json_utilities.py
@@ -1,6 +1,18 @@
 import json
 
+import jsonschema
+
 
 def open_json(file_path):
     with open(file_path) as fp:
         return json.load(fp)
+
+
+def validate_json_schema(json_data, dataset_schema):
+    try:
+        jsonschema.validate(json_data, schema=dataset_schema)
+        assert False
+    except jsonschema.ValidationError as e:
+        error_message = f"error: {e.message}. offending element: {e.json_path}"
+        print(error_message)
+        assert True
