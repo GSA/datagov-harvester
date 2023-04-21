545fe0a00cdb2d1f66ae2349e0a0c24f4f22fb6c
Reid Hewitt <sam.hew.r@gmail.com>
2023-04-21 10:05:04 -0600

    refactor output.

--- datagovharvester/validate/dcat_us.py
+++ datagovharvester/validate/dcat_us.py
@@ -1,13 +1,15 @@
 import jsonschema
 
 
-def validate_json_schema(json_data, dataset_schema, valid_flag):
-    # valid_flag: invalid = False; valid = True
-    output = valid_flag
+def validate_json_schema(json_data, dataset_schema):
+    success = None
+    error_message = ""
+
     try:
         jsonschema.validate(json_data, schema=dataset_schema)
+        success = True
     except jsonschema.ValidationError as e:
         error_message = f"error: {e.message}. offending element: {e.json_path}"
         print(error_message)
-        output = not valid_flag
-    return output
+        success = False
+    return success, error_message
