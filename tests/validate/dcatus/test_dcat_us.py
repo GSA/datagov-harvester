6d955013a572df7b77b9d69046c243f708dbb88c
Reid Hewitt <sam.hew.r@gmail.com>
2023-04-21 10:05:14 -0600

    refactor assertions.

--- tests/validate/dcatus/test_dcat_us.py
+++ tests/validate/dcatus/test_dcat_us.py
@@ -4,9 +4,9 @@ from datagovharvester.validate.dcat_us import validate_json_schema
 def test_numerical_title(open_catalog_schema, open_numerical_title_json):
     dataset_schema = open_catalog_schema
     json_data = open_numerical_title_json
-    valid_flag = False
 
-    assert validate_json_schema(json_data, dataset_schema, valid_flag)
+    result = validate_json_schema(json_data, dataset_schema)
+    assert result[0] == False
 
 
 def test_collection_1_parent_2_children(
@@ -14,25 +14,25 @@ def test_collection_1_parent_2_children(
 ):
     dataset_schema = open_catalog_schema
     json_data = open_collection_1_parent_2_children_json
-    valid_flag = True
 
-    assert validate_json_schema(json_data, dataset_schema, valid_flag)
+    result = validate_json_schema(json_data, dataset_schema)
+    assert result[0] == True
 
 
 def test_missing_catalog(open_catalog_schema, open_missing_catalog_json):
     dataset_schema = open_catalog_schema
     json_data = open_missing_catalog_json
-    valid_flag = False
 
-    assert validate_json_schema(json_data, dataset_schema, valid_flag)
+    result = validate_json_schema(json_data, dataset_schema)
+    assert result[0] == False
 
 
 def test_ny(open_catalog_schema, open_ny_json):
     dataset_schema = open_catalog_schema
     json_data = open_ny_json
-    valid_flag = False
 
-    assert validate_json_schema(json_data, dataset_schema, valid_flag)
+    result = validate_json_schema(json_data, dataset_schema)
+    assert result[0] == False
 
 
 def test_missing_identifier_title(
@@ -40,41 +40,41 @@ def test_missing_identifier_title(
 ):
     dataset_schema = open_catalog_schema
     json_data = open_missing_identifier_title_json
-    valid_flag = False
 
-    assert validate_json_schema(json_data, dataset_schema, valid_flag)
+    result = validate_json_schema(json_data, dataset_schema)
+    assert result[0] == False
 
 
 def test_usda_gov(open_catalog_schema, open_usda_gov_json):
     dataset_schema = open_catalog_schema
     json_data = open_usda_gov_json
-    valid_flag = True
 
-    assert validate_json_schema(json_data, dataset_schema, valid_flag)
+    result = validate_json_schema(json_data, dataset_schema)
+    assert result[0] == True
 
 
 def test_arm(open_catalog_schema, open_arm_json):
     dataset_schema = open_catalog_schema
     json_data = open_arm_json
-    valid_flag = True
 
-    assert validate_json_schema(json_data, dataset_schema, valid_flag)
+    result = validate_json_schema(json_data, dataset_schema)
+    assert result[0] == True
 
 
 def test_large_spatial(open_catalog_schema, open_large_spatial_json):
     dataset_schema = open_catalog_schema
     json_data = open_large_spatial_json
-    valid_flag = True
 
-    assert validate_json_schema(json_data, dataset_schema, valid_flag)
+    result = validate_json_schema(json_data, dataset_schema)
+    assert result[0] == True
 
 
 def test_reserved_title(open_catalog_schema, open_reserved_title_json):
     dataset_schema = open_catalog_schema
     json_data = open_reserved_title_json
-    valid_flag = True
 
-    assert validate_json_schema(json_data, dataset_schema, valid_flag)
+    result = validate_json_schema(json_data, dataset_schema)
+    assert result[0] == True
 
 
 def test_collection_2_parent_4_children(
@@ -82,22 +82,22 @@ def test_collection_2_parent_4_children(
 ):
     dataset_schema = open_catalog_schema
     json_data = open_collection_2_parent_4_children_json
-    valid_flag = True
 
-    assert validate_json_schema(json_data, dataset_schema, valid_flag)
+    result = validate_json_schema(json_data, dataset_schema)
+    assert result[0] == True
 
 
 def test_geospatial(open_catalog_schema, open_geospatial_json):
     dataset_schema = open_catalog_schema
     json_data = open_geospatial_json
-    valid_flag = True
 
-    assert validate_json_schema(json_data, dataset_schema, valid_flag)
+    result = validate_json_schema(json_data, dataset_schema)
+    assert result[0] == True
 
 
 def test_null_spatial(open_catalog_schema, open_null_spatial_json):
     dataset_schema = open_catalog_schema
     json_data = open_null_spatial_json
-    valid_flag = True
 
-    assert validate_json_schema(json_data, dataset_schema, valid_flag)
+    result = validate_json_schema(json_data, dataset_schema)
+    assert result[0] == True
