217df1d147125d8848f5e13c4b70ca4a2dc256f0
Reid Hewitt <sam.hew.r@gmail.com>
2023-05-09 08:33:01 -0600

    add try/catch and update return.

--- datagovharvester/utils/json_utilities.py
+++ datagovharvester/utils/json_utilities.py
@@ -2,5 +2,13 @@ import json
 
 
 def open_json(file_path):
-    with open(file_path) as fp:
-        return json.load(fp)
+    json_data = None
+    error_message = None
+
+    try:
+        with open(file_path) as fp:
+            json_data = json.load(fp)
+    except Exception as e:
+        error_message = e
+
+    return json_data, error_message
