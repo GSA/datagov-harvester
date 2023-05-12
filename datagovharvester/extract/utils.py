eba9988268c9a4f9355444c3d8d1de5eff1c8090
Reid Hewitt <sam.hew.r@gmail.com>
2023-05-12 09:27:35 -0600

    moved dcatus extract to own module.

--- datagovharvester/extract/utils.py
+++ datagovharvester/extract/utils.py
@@ -1,9 +1,4 @@
 import requests
-import json
-from datagovharvester.utils.s3_utilities import (
-    upload_dcatus_to_S3,
-    create_s3_payload,
-)
 
 
 def download_catalog(url):
@@ -29,16 +24,3 @@ def fetch_url(url):
     data = resp.json()
 
     return data, success
-
-
-def extract_catalog(url, job_id, S3_client, bucket_name):
-    data, fetch_success = fetch_url(url)
-
-    for idx, record in enumerate(data["dataset"]):
-        record = json.dumps(record)
-        key_name = f"{job_id}_{idx}_extract.json"
-        s3_payload = create_s3_payload(record, bucket_name, key_name)
-
-        upload_data, upload_error_message = upload_dcatus_to_S3(S3_client, s3_payload)
-
-    return upload_data, upload_error_message
