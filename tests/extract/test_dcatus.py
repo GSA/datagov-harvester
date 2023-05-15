8c2acaf0c670571de15b7e94255de38e4b2d3abe
Reid Hewitt <sam.hew.r@gmail.com>
2023-05-15 09:21:32 -0600

    refactor.

--- tests/extract/test_dcatus.py
+++ tests/extract/test_dcatus.py
@@ -1,4 +1,4 @@
-from datagovharvester.extract.dcatus import extract_dcatus_catalog
+from datagovharvester.extract.dcatus import extract_json_catalog
 
 
 def test_extract_dcatus(get_dcatus_job, create_client, create_bucket):
@@ -7,11 +7,11 @@ def test_extract_dcatus(get_dcatus_job, create_client, create_bucket):
     S3_client, S3_msg = create_client
     bucket_name, bucket_msg = create_bucket
 
-    final_res, final_error_msg = extract_dcatus_catalog(
+    extract_erros = extract_json_catalog(
         get_dcatus_job["url"], S3_client, get_dcatus_job["job_id"], bucket_name
     )
 
-    if final_error_msg is None:
+    if len(extract_erros) == 0:
         success = True
 
     assert success
