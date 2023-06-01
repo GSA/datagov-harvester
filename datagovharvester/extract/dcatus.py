0a6c08677b3b93cc44a2b30291ec9585f9a8414c
Reid Hewitt <sam.hew.r@gmail.com>
2023-06-01 09:58:40 -0600

    refactor. remove store_record_in_s3 function entirely.

--- datagovharvester/extract/dcatus.py
+++ datagovharvester/extract/dcatus.py
@@ -1,57 +1,19 @@
-from .utils import download_catalog
 import json
-from datagovharvester.utils.s3_utilities import (
-    upload_to_S3,
-    create_s3_payload,
-)
+from datagovharvester import content_types, extract_feat_name
+from datagovharvester.utils.s3_utilities import create_s3_upload_data
 
-def store_record_in_s3( record, source_id, job_id, record_idx, S3_client, 
-    key_name, bucket_name ):
-    """ store the input record in a s3 bucket
-    record (dict)               :   dcatus record
-    source_id (str)             :   uuid 
-    job_id (str)                :   uuid
-    record_idx (int)            :   index position of record in records list
-    S3_client (boto3.client)    :   boto3 s3 client. 
-    bucket_name (str)           :   name of bucket to store data. 
+# ruff: noqa: F841
+
+def parse_catalog( catalog, job_info ):
+    """ parse the catalog and yield each record as an S3 data upload dict
+    catalog (dict)  :   dcatus catalog json
+    job_info (dict) :   info on the job ( e.g. source_id, job_id, url )
     """
-    try:
-        record = json.dumps(record)
-        s3_payload = create_s3_payload(record, bucket_name, key_name)
-        upload_to_S3(S3_client, s3_payload)
-        return key_name
-    except Exception as e:
-        return e
-
-def extract_json_catalog(url, source_id, job_id, S3_client, bucket_name):
-    """ download, extract, and store dcatus records in s3 bucket
-    url (str)                   :   download file path 
-    source_id (str)             :   uuid 
-    job_id (str)                :   uuid
-    S3_client (boto3.client)    :   boto3 s3 client. 
-    bucket_name (str)           :   name of bucket to store data. 
-    """
-
-    output = { "errors": [], "job_ids": [], "s3_paths": [] } 
-
-    try:
-        data = download_catalog(url)
-    except Exception as e:
-        # do something with the error
-        return e
-
-    if not data.get("dataset", None):
-        # do something with the error
-        return "no dataset key"
-
-    for idx, record in enumerate(data["dataset"]):
+    for idx, record in enumerate(catalog["dataset"]):
         try:
-            key_name = f"/{source_id}/{job_id}/{idx}.json"
-            store_record_in_s3( record, source_id, job_id, idx, S3_client, 
-                key_name, bucket_name )
-            output["s3_paths"].append( key_name ) 
-            output["job_ids"].append( job_id ) 
+            record = json.dumps(record)
+            key_name = "{}/{}/{}/{}.json".format( extract_feat_name, 
+                job_info["source_id"], job_info["job_id"], idx )
+            yield create_s3_upload_data(record, key_name, content_types["json"])
         except Exception as e:
-            output["errors"].append( e ) 
-
-    return output
+            pass 
