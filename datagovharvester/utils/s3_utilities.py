8c71b89c924d2212963455ac7332c1e4fcd88d8b
Reid Hewitt <sam.hew.r@gmail.com>
2023-06-01 09:59:32 -0600

    refactor and update docstrings. use config bucket name.

--- datagovharvester/utils/s3_utilities.py
+++ datagovharvester/utils/s3_utilities.py
@@ -1,5 +1,6 @@
 import boto3
 import botocore
+from datagovharvester import bucket_name
 
 # ruff: noqa: F841
 
@@ -9,29 +10,28 @@ def create_s3_client(s3_config):
     """
     try:
         return boto3.client("s3", **s3_config)
-    except botocore.exceptions.ClientError as e: #noqa
+    except botocore.exceptions.ClientError as e:
         pass 
 
-def create_s3_payload(json_str, bucket_name, key_name):
-    """ create s3 payload to be added to the input bucket
+def create_s3_upload_data(body, key_name, content_type):
+    """ create s3 data to be uploaded to the default bucket
     json_str (str)      :   data to be placed in s3 bucket as json string. 
-    bucket_name (str)   :   name of bucket to store the data. 
     key_name (str)      :   name of the file to be placed in the s3 bucket. 
     """
     return {
-        "Body": json_str,
+        "Body": body,
         "Bucket": bucket_name,
         "Key": key_name,
-        "ContentType": "application/json",
+        "ContentType": content_type,
     }
 
 
-def upload_to_S3(S3, s3_payload):
+def upload_to_S3(S3, s3_upload_data):
     """ store the s3 payload 
     S3 (boto3 client)   :   boto3 S3 client
-    s3_payload (dict)   :   payload to be stored in s3 bucket.
+    s3_upload_data (dict)   :   payload to be stored in s3 bucket.
     """
     try:
-        return S3.put_object(**s3_payload)
+        return S3.put_object(**s3_upload_data)
     except Exception as e:
         pass 
