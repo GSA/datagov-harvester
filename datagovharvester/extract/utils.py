f52a8c15701086f7cbad9bd1675765013f9f6d0f
Reid Hewitt <sam.hew.r@gmail.com>
2023-06-01 09:59:00 -0600

    rename function. add raise exceptions.

--- datagovharvester/extract/utils.py
+++ datagovharvester/extract/utils.py
@@ -2,21 +2,23 @@ import requests
 import json
 
 
-def download_catalog(url):
+def download_json(url):
     """ download file and pull json from response
     url (str)   :   path to the file to be downloaded.
     """
     try:
         resp = requests.get(url)
     except requests.exceptions.RequestException as e:
-        return e
+        raise Exception(e)
+    except requests.exceptions.JSONDecodeError as e:
+        raise Exception(e) 
 
     if resp.status_code != 200:
-        return None
+        raise Exception("non-200 status code")
 
     try:
         data = resp.json()
     except json.JSONDecodeError as e:
-        return e
+        raise Exception(e)
 
     return data
