5c14cf134e42b442c17c74da10792a2b13a40583
Reid Hewitt <sam.hew.r@gmail.com>
2023-06-15 08:27:23 -0600

    wait until start status is finished.

--- tests/s3/test_s3.py
+++ tests/s3/test_s3.py
@@ -1,7 +1,12 @@
+from time import sleep 
 
 def test_localstack_is_healthy(get_docker_api_client):
 
     api_client = get_docker_api_client
     inspect_results = api_client.inspect_container("localstack-container")
+
+    while inspect_results["State"]["Health"]["Status"] == "starting":
+        sleep(5)
+        inspect_results = api_client.inspect_container("localstack-container")
 
     assert inspect_results["State"]["Health"]["Status"] == "healthy"
