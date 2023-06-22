52212dd1a1fda0a0ba5e0d695a1287a2f6374e16
Reid Hewitt <sam.hew.r@gmail.com>
2023-06-22 11:25:00 -0600

    add needed imports and s3 record verification

--- tests/extract/test_dcatus.py
+++ tests/extract/test_dcatus.py
@@ -1,6 +1,13 @@
+from pathlib import Path
+from deepdiff import DeepDiff
+import json 
+
 from harvester import bucket_name
 from harvester.extract import main
+from harvester.utils.s3 import get_s3_object
+from harvester.utils.json import open_json
 
+TEST_DIR = Path(__file__).parents[1]
 
 def test_extract_dcatus(get_dcatus_job, create_client):
     """download dcat-us json file and store result in s3 bucket.
@@ -11,8 +18,18 @@ def test_extract_dcatus(get_dcatus_job, create_client):
     S3_client = create_client
     S3_client.create_bucket(Bucket=bucket_name)
 
-    assert main(get_dcatus_job, S3_client)
+    res = main(get_dcatus_job, S3_client)
+    assert res 
 
+    # check if the s3 record matches the test record
+    dcatus_json = TEST_DIR / "harvest-sources" / "dcatus.json"
+    dcatus_record_obj = open_json( dcatus_json )["dataset"][0]
+
+    s3_record_obj = get_s3_object( S3_client, "test-bucket", res["s3_paths"][0] )
+    s3_record_obj = s3_record_obj["Body"].read().decode("utf-8")
+    s3_record_obj = json.loads( s3_record_obj ) 
+
+    assert len( DeepDiff( dcatus_record_obj, s3_record_obj ) ) == 0 
 
 def test_extract_bad_url(get_bad_url, create_client):
     """attempt to download a bad url.
@@ -22,10 +39,15 @@ def test_extract_bad_url(get_bad_url, create_client):
 
     S3_client = create_client
     S3_client.create_bucket(Bucket=bucket_name)
-
+ 
     assert str(main(get_bad_url, S3_client)) == "non-200 status code"
 
+    paginator = S3_client.get_paginator("list_objects_v2")
+    page_iterator = paginator.paginate(Bucket="test-bucket")
 
+    for page in page_iterator:
+        assert page["KeyCount"] == 0
+    
 def test_extract_bad_json(get_bad_json, create_client):
     """attempt to download a bad url.
     get_bad_json (dict)             :   fixture containing job data with bad json
@@ -41,7 +63,6 @@ def test_extract_bad_json(get_bad_json, create_client):
     )
     assert str(main(get_bad_json, S3_client)) == error
 
-
 def test_extract_no_dataset_key(get_no_dataset_key_dcatus_json, create_client):
     """attempt to download a invalid dcatus catalog.
     get_no_dataset_key_dcatus_json (dict)
@@ -53,11 +74,10 @@ def test_extract_no_dataset_key(get_no_dataset_key_dcatus_json, create_client):
     S3_client.create_bucket(Bucket=bucket_name)
 
     assert main(get_no_dataset_key_dcatus_json, S3_client) == "invalid dcatus catalog"
-
-
-def test_bad_s3_client(create_bad_client):
+   
+def test_bad_s3_client( create_bad_client ):
     """attempt to create an S3 client using a bad url endpoint
-    create_bad_client (boto3.client)    : S3 client object.
+    create_bad_client (boto3.client)    : S3 client object. 
     """
 
     S3_client = create_bad_client
