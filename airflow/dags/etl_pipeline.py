12f7469e004899d760e269cdce955da4365c8581
Reid Hewitt <sam.hew.r@gmail.com>
2023-08-28 10:49:12 -0600

    add dynamic dag creation for test collection of sources.

--- airflow/dags/etl_pipeline.py
+++ airflow/dags/etl_pipeline.py
@@ -1,30 +1,9 @@
-from airflow import DAG
-from datetime import datetime, timedelta
+from datetime import datetime
 from airflow.decorators import dag, task
-import time
 import json
-
-# from uuid import uuid4
 from pathlib import Path
 from jsonschema import Draft202012Validator
 import requests
-
-# s3 stuff
-
-# BUCKET_NAME = "test-bucket"
-# S3_CONFIG = {
-#     "aws_access_key_id": "_placeholder",
-#     "aws_secret_access_key": "_placeholder",
-#     "region_name": "us-east-1",
-#     "endpoint_url": "http://127.0.0.1:4566",
-# }
-# S3_CLIENT = create_s3_client(S3_CONFIG)
-# FILEKEY = str(uuid4())
-
-"""
-    - test passing data via xcom for all tasks
-    - don't worry about s3 for right now
-"""
 
 ROOT_DIR = Path(__file__).parents[2]
 SCHEMA_DIR = ROOT_DIR / "schemas"
@@ -34,74 +13,42 @@ DATASET_SCHEMA = SCHEMA_DIR / "dataset.json"
 with open(DATASET_SCHEMA) as json_file:
     dcatus_dataset_schema = json.load(json_file)
 
+daily_data_url = "https://catalog.data.gov/api/action/package_search?fq=dataset_type:harvest%20AND%20source_type:datajson%20AND%20frequency:DAILY&rows=1000"
+res = requests.get(daily_data_url).json()
 
-@dag(
-    "datagov_etl_pipeline",
-    start_date=datetime(year=2023, month=2, day=5),
-    catchup=False,
-    tags=["etl"],
-    schedule_interval=timedelta(minutes=2),
-)
-def etl_pipeline():
-    @task(task_id="extract_dcatus")
-    def extract():
-        url = "https://github.com/GSA/ckanext-datajson/blob/main/ckanext/datajson/tests/datajson-samples/large-spatial.data.json"
-        # url = "https://github.com/GSA/ckanext-datajson/blob/main/ckanext/datajson/tests/datajson-samples/ny.data.json"
-        res = requests.get(url)
-        if res.status_code == 200:
-            data = res.json()["payload"]["blob"]["rawLines"]
-            return json.loads("".join(data))["dataset"]
+for harvest_source in res["result"]["results"]:
+    frequency = harvest_source["frequency"]
+    title = "_".join(map(str.lower, harvest_source["title"].split()))
+    url = harvest_source["url"]
 
-        # try:
-        #     catalog = download_json(job_info["url"])
-        # except Exception as e:
-        #     # do something with e
-        #     return e
+    @dag(
+        f"{title}_workflow",
+        start_date=datetime(year=2023, month=2, day=5),
+        schedule_interval=f"@{frequency.lower()}",
+    )
+    def etl_pipeline():
+        @task(task_id="extract_dcatus")
+        def extract(url):
+            return requests.get(url).json()
 
-        # # check schema
-        # if not is_dcatus_schema(catalog):
-        #     return "invalid dcatus catalog"
-
-        # # parse catalog and upload records
-        # try:
-        #     for record_info in parse_catalog(catalog, job_info):
-        #         upload_to_S3(S3_client, record_info)
-        #         output["s3_paths"].append(record_info["Key"])
-        # except Exception as e:
-        #     return False
-
-        # return output
-
-    @task(task_id="validate_dcatus")
-    def validate(dcatus_record):
-        validator = Draft202012Validator(dcatus_dataset_schema)
-        try:
+        @task(task_id="validate_dcatus")
+        def validate(dcatus_record):
+            validator = Draft202012Validator(dcatus_dataset_schema)
             validator.validate(dcatus_record)
             return dcatus_record
-        except:
-            return False
 
-    @task(task_id="transform_dcatus")
-    def transform():
-        return
+        @task(task_id="load_dcatus")
+        def load(dcatus_record, ti=None):
+            print("grabbing validation catalog via xcom")
+            return ti.xcom_pull(task_ids="validate_dcatus")
 
-    @task(task_id="load_dcatus")
-    def load():
-        dcatus = get_s3_object(S3_CLIENT, BUCKET_NAME, FILEKEY)
-        for idx, record in enumerate(dcatus["dataset"]):
-            upload_data = create_s3_upload_data(
-                json.dumps(record), BUCKET_NAME, FILEKEY + str(idx), "application/json"
-            )
-            upload_to_S3(S3_CLIENT, upload_data)
+        # @task(task_id="extract_error_handler")
+        # def extract_error_handler(ti=None):
+        #     res = ti.xcom_pull(task_ids="extract_dcatus")
+        #     print(res)
 
-    # extract() >> transform() >> load()
+        # extract() >> extract_error_handler()
 
-    # list_filenames = extract()
-    # validated = list_filenames.output.map(validate)
+        load.expand(dcatus_record=validate.expand(dcatus_record=extract(url)))
 
-    validate.expand(dcatus_record=extract())
-
-    # validated.output.map(load)
-
-
-_ = etl_pipeline()
+    _ = etl_pipeline()
