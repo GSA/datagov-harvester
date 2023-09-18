ed16f68ca77bff99aafa8d911a3389f2af1a9083
Reid Hewitt <sam.hew.r@gmail.com>
2023-09-18 12:48:42 -0600

    create CUD ckan module

---
+++ harvester/load/ckan.py
@@ -0,0 +1,24 @@
+import requests
+import ckanapi
+
+
+def create_ckan_entrypoint(url, api_key):
+    return ckanapi.RemoteCKAN(url, apikey=api_key)
+
+
+def create_ckan_package(ckan, package_data):
+    return ckan.action.package_create(**package_data)
+
+
+def patch_ckan_package(ckan, patch_data):
+    # partially updates the package
+    return ckan.action.package_patch(**patch_data)
+
+
+def update_ckan_package(ckan, update_data):
+    # fully replaces the package
+    return ckan.action.package_update(**update_data)
+
+
+def delete_ckan_package(ckan, package_data):
+    return ckan.action.package_delete(**package_data)
