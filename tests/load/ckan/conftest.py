4017656a69a16744ec6fdac546154f4f49db16f6
Reid Hewitt <sam.hew.r@gmail.com>
2023-09-20 12:15:43 -0600

    update ckan entrypoint fixture for local and gha testing. add purge fixture.

--- tests/load/ckan/conftest.py
+++ tests/load/ckan/conftest.py
@@ -1,13 +1,17 @@
 import pytest
 from harvester.load.ckan import create_ckan_entrypoint
-import credentials
+import os
 
 
 @pytest.fixture
 def ckan_entrypoint():
-    return create_ckan_entrypoint(
-        "http://localhost:5000", credentials.ckan_catalog_dev_api_key
-    )
+    catalog_dev_api_key = os.getenv("CKAN_API_TOKEN_DEV")  # gha
+    if catalog_dev_api_key is None:  # local
+        import credentials
+
+        catalog_dev_api_key = credentials.ckan_catalog_dev_api_key
+
+    return create_ckan_entrypoint("https://catalog-dev.data.gov/", catalog_dev_api_key)
 
 
 @pytest.fixture
@@ -18,9 +22,9 @@ def test_ckan_package_id():
 @pytest.fixture
 def test_ckan_package(test_ckan_package_id):
     return {
-        "name": "package_test",
-        "title": "title_test",
-        "owner_org": "test_org",
+        "name": "test_package_name",
+        "title": "title_test_name",
+        "owner_org": "test",
         "tag_string": "test",
         "id": test_ckan_package_id,
     }
@@ -34,3 +38,8 @@ def test_ckan_update_package(test_ckan_package_id):
 @pytest.fixture
 def test_ckan_patch_package(test_ckan_package_id):
     return {"author_email": "test@gmail.com", "id": test_ckan_package_id}
+
+
+@pytest.fixture
+def test_ckan_purge_package(test_ckan_package_id):
+    return {"id": test_ckan_package_id}
