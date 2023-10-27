fd8d81daebe859ef3a786c3609e17a4bb7d8d90f
Tyler Burton <tyler.burton@gsa.gov>
2023-10-26 19:33:31 -0500

    working version of airflow standalone pushed to cloud.gov development space

---
+++ scripts/scale-up.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# ARRAY=( "cow:moo"
+#         "dinosaur:roar"
+#         "bird:chirp"
+#         "bash:rock" )
+
+# for animal in "${ARRAY[@]}" ; do
+#     printf "${animal%%:*} likes to ${animal##*:}\n"
+# done
+
+APPS=(
+    "test-airflow-dagprocessor"
+    "test-airflow-flower"
+    "test-airflow-scheduler"
+    "test-airflow-triggerer"
+    "test-airflow-webserver"
+    "test-airflow-worker"
+)
+
+# SERVICES=(
+# )
+
+for app in "${APPS[@]}" ; do
+    cf scale "${app%%:*}" -i 1
+done
