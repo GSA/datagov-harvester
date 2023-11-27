1ff1ad1c59af46214be787052798f321fe9ca9b0
Tyler Burton <tyler.burton@gsa.gov>
2023-11-27 15:10:03 -0600

    updates README; removes local executor configs; tunes logging level from DEBUG to INFO

--- get_service_key.sh
+++ get_service_key.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#
+# For connecting locally to Cloud.gov Redis instance
+# https://cloud.gov/docs/services/aws-elasticache/#connecting-to-your-elasticache-service-locally
+#
+
 elasticache_credentials=`cf service-key airflow-test-redis airflow-test-service-key | tail -n +3`
 echo export elasticache_hostname=`echo "${elasticache_credentials}" | jq -r '.credentials.hostname'`
 echo export elasticache_port=`echo "${elasticache_credentials}" | jq -r '.credentials.port'`
