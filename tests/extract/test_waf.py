16185b6f79d04008892d11735962d5e5763f041b
Reid Hewitt <sam.hew.r@gmail.com>
2023-12-18 11:42:10 -0700

    include waf download in test.

--- tests/extract/test_waf.py
+++ tests/extract/test_waf.py
@@ -1,6 +1,9 @@
-from harvester.extract.waf import traverse_waf
+from harvester.extract.waf import traverse_waf, download_waf
 
 
 def test_traverse_waf(get_waf_url):
     files = traverse_waf(get_waf_url, filters=["../", "dcatus/"])
     assert len(files) == 7
+
+    downloaded_files = download_waf(files)
+    assert len(downloaded_files) == 7
