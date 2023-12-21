461690277401cffbf75359458c4a670b2454017f
Reid Hewitt <sam.hew.r@gmail.com>
2023-12-21 15:51:48 -0700

    include more functions.

--- harvester/__init__.py
+++ harvester/__init__.py
@@ -1,12 +1,33 @@
 # TODO: maybe turn off this ruff ignore?
 # ruff: noqa: F405, F403
 
-__all__ = ["compare", "extract", "load", "transform", "validate", "utils"]
+__all__ = [
+    "compare",
+    "extract",
+    "traverse_waf",
+    "download_waf",
+    "load",
+    "create_ckan_package",
+    "update_ckan_package",
+    "patch_ckan_package",
+    "purge_ckan_package",
+    "dcatus_to_ckan",
+    "transform",
+    "validate",
+    "utils",
+]
 
 # TODO these imports will need to be updated to ensure a consistent api
 from .compare import compare
-from .extract import extract
-from .load import load
+from .extract import extract, traverse_waf, download_waf
+from .load import (
+    load,
+    create_ckan_package,
+    update_ckan_package,
+    patch_ckan_package,
+    purge_ckan_package,
+    dcatus_to_ckan,
+)
 from .transform import transform
 from .utils import *
 from .validate import *
