1609b80adcc854c18a9e1c16ce64811df9d06e93
Reid Hewitt <sam.hew.r@gmail.com>
2023-12-21 15:05:30 -0700

    create integration tests.

---
+++ tests/integration/transform/test_transform_int.py
@@ -0,0 +1,19 @@
+from harvester.transform import transform
+from harvester.extract import traverse_waf, download_waf
+
+
+def test_transform(mock_transform):
+    """tests transform"""
+
+    files = traverse_waf("http://localhost:80", filters=["../", "dcatus/"])
+    downloaded_files = download_waf(files)
+
+    for file in downloaded_files:
+        data = {
+            "file": file["content"],
+            "reader": "fgdc",
+            "writer": "iso19115_3",
+        }
+
+        transform_response = mock_transform(data)
+        assert transform_response["transformed_data"] is not None
