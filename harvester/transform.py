abe4d97e9e8a024fa14ea73daf96c3d4f10d3755
Reid Hewitt <sam.hew.r@gmail.com>
2023-12-21 10:50:32 -0700

    add transformation function content.

--- harvester/transform.py
+++ harvester/transform.py
@@ -1,11 +1,18 @@
 import logging
+import requests
+import json
 
 logger = logging.getLogger("harvester")
 
 
-# stub, TODO complete
-def transform(transform_obj):
+def transform(url, transform_obj):
     """Transforms records"""
     logger.info("Hello from harvester.transform()")
+
+    res = requests.post(url, transform_obj)
+
+    if res.status_code == 200:
+        data = json.loads(res.content.decode("utf-8"))
+        transform_obj["transformed_data"] = data["writerOutput"]
 
     return transform_obj
