661225bcbea4297d053b5f42f422720e8ee9b070
Reid Hewitt <sam.hew.r@gmail.com>
2023-12-27 15:06:07 -0700

    move data to fixture and refactor.

---
+++ tests/unit/compare/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+@pytest.fixture
+def data_sources():
+    harvest_source = {
+        "1": "de955c1b-fa16-4b84-ad6c-f891ba276056",  # update
+        "2": "6d500ebc-19f8-4541-82b0-f02ad24c82e3",  # do nothing
+        "3": "9aeef506-fbc4-42e4-ad27-c2e7e9f0d1c5",  # create
+    }
+
+    ckan_source = {
+        "1": "fcd3428b-0ba7-48da-951d-fe44606be556",
+        "2": "6d500ebc-19f8-4541-82b0-f02ad24c82e3",
+        "4": "dae9b42c-cfc5-4f71-ae97-a5b75234b14f",  # delete
+    }
+
+    return harvest_source, ckan_source
--- tests/unit/compare/test_compare.py
+++ tests/unit/compare/test_compare.py
@@ -1,22 +1,10 @@
 from harvester.compare import compare
 
 
-def test_compare():
+def test_compare(data_sources):
     """tests compare"""
 
-    harvest_source = {
-        "1": "de955c1b-fa16-4b84-ad6c-f891ba276056",  # update
-        "2": "6d500ebc-19f8-4541-82b0-f02ad24c82e3",  # do nothing
-        "3": "9aeef506-fbc4-42e4-ad27-c2e7e9f0d1c5",  # create
-    }
-
-    ckan_source = {
-        "1": "fcd3428b-0ba7-48da-951d-fe44606be556",
-        "2": "6d500ebc-19f8-4541-82b0-f02ad24c82e3",
-        "4": "dae9b42c-cfc5-4f71-ae97-a5b75234b14f",  # delete
-    }
-
-    compare_res = compare(harvest_source, ckan_source)
+    compare_res = compare(*data_sources)
 
     assert len(compare_res["create"]) == 1
     assert len(compare_res["update"]) == 1
