851382f519a7c653756324848545b142819a282c
Reid Hewitt <sam.hew.r@gmail.com>
2024-01-03 12:48:53 -0700

    move fixture to integration test

--- tests/unit/load/ckan/conftest.py
+++ tests/unit/load/ckan/conftest.py
@@ -10,15 +10,15 @@ TEST_DIR = Path(__file__).parents[3]
 HARVEST_SOURCES = TEST_DIR / "harvest-sources"
 
 
-@pytest.fixture
-def ckan_entrypoint():
-    catalog_dev_api_key = os.getenv("CKAN_API_TOKEN_DEV")  # gha
-    if catalog_dev_api_key is None:  # local
-        import credentials
+# @pytest.fixture
+# def ckan_entrypoint():
+#     catalog_dev_api_key = os.getenv("CKAN_API_TOKEN_DEV")  # gha
+#     if catalog_dev_api_key is None:  # local
+#         import credentials
 
-        catalog_dev_api_key = credentials.ckan_catalog_dev_api_key
+#         catalog_dev_api_key = credentials.ckan_catalog_dev_api_key
 
-    return create_ckan_entrypoint("https://catalog-dev.data.gov/", catalog_dev_api_key)
+#     return create_ckan_entrypoint("https://catalog-dev.data.gov/", catalog_dev_api_key)
 
 
 @pytest.fixture
@@ -33,7 +33,7 @@ def test_dcatus_catalog():
 
 @pytest.fixture
 def test_ckan_package(test_ckan_package_id, test_dcatus_catalog):
-    ckan_dataset = dcatus_to_ckan(test_dcatus_catalog)
+    ckan_dataset = dcatus_to_ckan(test_dcatus_catalog, "test_harvest_source_name")
     ckan_dataset["id"] = test_ckan_package_id
     return ckan_dataset
 
