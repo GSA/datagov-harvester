64c7d16ab97e2553d10c958bb1f543ec30a9f69f
Reid Hewitt <sam.hew.r@gmail.com>
2024-01-03 12:48:38 -0700

    add real comparison fixture and test

--- tests/unit/compare/conftest.py
+++ tests/unit/compare/conftest.py
@@ -1,8 +1,15 @@
 import pytest
+from pathlib import Path
+import json
+from harvester.utils.json import open_json
+from harvester.utils.util import sort_dataset, dataset_to_hash
+
+TEST_DIR = Path(__file__).parents[2]
+HARVEST_SOURCES = TEST_DIR / "harvest-sources"
 
 
 @pytest.fixture
-def data_sources():
+def artificial_data_sources():
     harvest_source = {
         "1": "de955c1b-fa16-4b84-ad6c-f891ba276056",  # update
         "2": "6d500ebc-19f8-4541-82b0-f02ad24c82e3",  # do nothing
@@ -14,5 +21,39 @@ def data_sources():
         "2": "6d500ebc-19f8-4541-82b0-f02ad24c82e3",
         "4": "dae9b42c-cfc5-4f71-ae97-a5b75234b14f",  # delete
     }
+
+    return harvest_source, ckan_source
+
+
+@pytest.fixture
+def data_sources():
+    harvest_source_datasets = open_json(
+        HARVEST_SOURCES / "dcatus" / "dcatus_compare.json"
+    )["dataset"]
+
+    harvest_source = {}
+    for d in harvest_source_datasets:
+        harvest_source[d["identifier"]] = dataset_to_hash(
+            sort_dataset(d)
+        )  # the extract needs to be sorted
+
+    ckan_source_datasets = open_json(
+        HARVEST_SOURCES / "dcatus" / "ckan_datasets_resp.json"
+    )["result"]["results"]
+
+    ckan_source = {}
+
+    for d in ckan_source_datasets:
+        orig_meta = None
+        orig_id = None
+        for e in d["extras"]:
+            if e["key"] == "dcat_metadata":
+                orig_meta = eval(e["value"], {"__builtins__": {}})
+            if e["key"] == "identifier":
+                orig_id = e["value"]
+
+        ckan_source[orig_id] = dataset_to_hash(
+            orig_meta
+        )  # the response is stored sorted
 
     return harvest_source, ckan_source
--- tests/unit/compare/test_compare.py
+++ tests/unit/compare/test_compare.py
@@ -1,12 +1,19 @@
 from harvester.compare import compare
 
 
-def test_compare(data_sources):
-    """tests compare"""
+def test_artificial_compare(artificial_data_sources):
+    """tests artificial datasets compare"""
 
-    compare_res = compare(*data_sources)
+    compare_res = compare(*artificial_data_sources)
 
     assert len(compare_res["create"]) == 1
     assert len(compare_res["update"]) == 1
     assert len(compare_res["delete"]) == 1
-    assert "2" not in compare_res
+
+
+def test_compare(data_sources):
+    compare_res = compare(*data_sources)
+
+    assert len(compare_res["create"]) == 1
+    assert len(compare_res["update"]) == 3
+    assert len(compare_res["delete"]) == 1
