685f585efed17203322929c99ae47e8944b5f7f3
Tyler Burton <tyler.burton@gsa.gov>
2024-02-01 16:13:00 -0600

    updates to script

--- scripts/load_test.py
+++ scripts/load_test.py
@@ -11,18 +11,19 @@ url = os.environ['SRC_URL']
 owner_org = os.environ['SRC_OWNER_ORG']
 source_type = os.environ['SRC_SOURCE_TYPE']
 
-print(title)
-print(url)
-print(owner_org)
-print(source_type)
+print('Running load test for the following harvest config')
+print(f'title: {title}')
+print(f'url: {url}')
+print(f'owner_org: {owner_org}')
+print(f'source_type: {source_type}')
 
 harvest_source = HarvestSource(
     title,
     url,
     owner_org,
     source_type
-)
+)   
 
-harvest_source.get_record_changes()
+harvest_source.get_record_changes() 
 harvest_source.synchronize_records()
 harvest_source.report()
