21197753274b333033d3ba9062716f9499bff8cc
Reid Hewitt <sam.hew.r@gmail.com>
2024-03-05 12:42:26 -0700

    update fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,22 @@
 from pathlib import Path
+from uuid import uuid4
 
 import pytest
 
 from harvester.utils import open_json
 
 HARVEST_SOURCES = Path(__file__).parents[0] / "harvest-sources"
+
+
+@pytest.fixture
+def bad_url_dcatus_config() -> dict:
+    return {
+        "_title": "test_harvest_source_title",
+        "_url": "http://localhost/dcatus/bad_url.json",
+        "_extract_type": "datajson",
+        "_owner_org": "example_organization",
+        "_job_id": str(uuid4()),
+    }
 
 
 @pytest.fixture
@@ -15,6 +27,18 @@ def dcatus_config() -> dict:
         "_url": "http://localhost/dcatus/dcatus.json",
         "_extract_type": "datajson",
         "_owner_org": "example_organization",
+        "_job_id": str(uuid4()),
+    }
+
+
+@pytest.fixture
+def invalid_dcatus_config() -> dict:
+    return {
+        "_title": "test_harvest_source_name",
+        "_url": "http://localhost/dcatus/missing_title.json",
+        "_extract_type": "datajson",
+        "_owner_org": "example_organization",
+        "_job_id": str(uuid4()),
     }
 
 
@@ -27,6 +51,7 @@ def waf_config() -> dict:
         "_extract_type": "waf-collection",
         "_owner_org": "example_organization",
         "_waf_config": {"filters": ["../", "dcatus/"]},
+        "_job_id": str(uuid4()),
     }
 
 
@@ -38,6 +63,7 @@ def dcatus_compare_config() -> dict:
         "_url": "http://localhost/dcatus/dcatus_compare.json",
         "_extract_type": "datajson",
         "_owner_org": "example_organization",
+        "_job_id": str(uuid4()),
     }
 
 
