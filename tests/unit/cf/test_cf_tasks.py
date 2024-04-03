a7686bcacd24af4d7fb50de44e449774d13e1bca
Reid Hewitt <sam.hew.r@gmail.com>
2024-04-03 09:16:47 -0600

    add unit test for cf

---
+++ tests/unit/cf/test_cf_tasks.py
@@ -0,0 +1,34 @@
+from unittest.mock import patch
+from harvester.utils import CFHandler
+
+
+class TestCFTasking:
+    @patch.object(CFHandler, "start_task")
+    def test_add_task(self, start_mock, cf_handler, dhl_cf_task_data):
+        start_mock.return_value = {"guuid": "test"}
+
+        assert cf_handler.start_task(**dhl_cf_task_data) is not None
+
+    @patch.object(CFHandler, "get_task")
+    def test_get_task(self, get_mock, cf_handler, dhl_cf_task_data):
+        get_mock.return_value = {"guuid": "test"}
+
+        task = cf_handler.get_task(dhl_cf_task_data["task_id"])
+        assert task is not None
+
+    @patch.object(CFHandler, "stop_task")
+    def test_cancel_task(self, stop_mock, cf_handler, dhl_cf_task_data):
+        stop_mock.return_value = {"guuid": "test"}
+
+        task = cf_handler.stop_task(dhl_cf_task_data["task_id"])
+        assert task is not None
+
+    @patch.object(CFHandler, "read_recent_app_logs")
+    def test_read_recent_task_logs(self, read_mock, cf_handler, dhl_cf_task_data):
+        read_mock.return_value = "recent information on the task"
+
+        logs = cf_handler.read_recent_app_logs(
+            dhl_cf_task_data["app_guuid"], dhl_cf_task_data["task_id"]
+        )
+
+        assert logs is not None
