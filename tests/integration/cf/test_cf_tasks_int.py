1d06f0c6030870e8e1cd0b23356387d56d4f0350
Reid Hewitt <sam.hew.r@gmail.com>
2024-04-22 11:19:09 -0600

    fix arg issue

--- tests/integration/cf/test_cf_tasks_int.py
+++ tests/integration/cf/test_cf_tasks_int.py
@@ -5,9 +5,7 @@ class TestCFTasking:
 
     def test_get_task(self, cf_handler, dhl_cf_task_data):
 
-        task = cf_handler.get_task(
-            dhl_cf_task_data["app_guuid"], dhl_cf_task_data["task_id"]
-        )
+        task = cf_handler.get_task(dhl_cf_task_data["task_id"])
         assert task is not None
 
     def test_get_all_app_tasks(self, cf_handler, dhl_cf_task_data):
