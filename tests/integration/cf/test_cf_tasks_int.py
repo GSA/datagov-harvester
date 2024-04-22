32baa6b8fbbfdcb19c888b48990006ca8689c21b
Reid Hewitt <sam.hew.r@gmail.com>
2024-04-22 16:33:46 -0600

    condense add, read, and delete into 1 test

--- tests/integration/cf/test_cf_tasks_int.py
+++ tests/integration/cf/test_cf_tasks_int.py
@@ -1,26 +1,25 @@
+from time import sleep
+
+
 class TestCFTasking:
-    def test_add_task(self, cf_handler, dhl_cf_task_data):
+    def test_crud_task(self, cf_handler, dhl_cf_task_data):
 
-        assert cf_handler.start_task(**dhl_cf_task_data) is not None
+        # start a new task
+        new_task = cf_handler.start_task(**dhl_cf_task_data)
+        sleep(2)
+        # retrieve that task via task guid
+        task = cf_handler.get_task(new_task["guid"])
 
-    def test_get_task(self, cf_handler, dhl_cf_task_data):
+        # read the recent logs of the task
+        logs = cf_handler.read_recent_app_logs(
+            dhl_cf_task_data["app_guuid"], task["guid"]
+        )
+        assert logs is not None
 
-        task = cf_handler.get_task(dhl_cf_task_data["task_id"])
-        assert task is not None
+        # cancel the task
+        cancelled_task = cf_handler.stop_task(task["guid"])
+        assert cancelled_task is not None
 
     def test_get_all_app_tasks(self, cf_handler, dhl_cf_task_data):
         tasks = cf_handler.get_all_app_tasks(dhl_cf_task_data["app_guuid"])
         assert tasks is not None
-
-    def test_cancel_task(self, cf_handler, dhl_cf_task_data):
-
-        task = cf_handler.stop_task(dhl_cf_task_data["task_id"])
-        assert task is not None
-
-    def test_read_recent_task_logs(self, cf_handler, dhl_cf_task_data):
-
-        logs = cf_handler.read_recent_app_logs(
-            dhl_cf_task_data["app_guuid"], dhl_cf_task_data["task_id"]
-        )
-
-        assert logs is not None
