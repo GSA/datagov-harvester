2362eec1f0136e9b4d39370cf36df145fd658728
Tyler Burton <tyler.burton@gsa.gov>
2024-07-08 17:45:25 -0500

    cleans up last job status logic

--- app/routes.py
+++ app/routes.py
@@ -257,7 +257,9 @@ def view_org_data(org_id: str):
             future_harvest_jobs[source.id] = job[0].date_created
     harvest_jobs = {}
     for source in sources:
-        job = db.get_first_harvest_jobs_by_filter({"harvest_source_id": source.id})
+        job = db.get_first_harvest_jobs_by_filter(
+            {"harvest_source_id": source.id, "status": "complete"}
+        )
         if job:
             harvest_jobs[source.id] = job
     data = {
