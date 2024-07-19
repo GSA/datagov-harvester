482637157518e8d8ee332f1557af80280d17c7cc
Tyler Burton <tyler.burton@gsa.gov>
2024-07-19 17:02:51 -0500

    test run

---
+++ scripts/fake.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_TOKEN = os.getenv("CKAN_API_TOKEN")
+FOO_VAR = os.getenv("FOO_VAR")
+SRC_TITLE = os.getenv("SRC_TITLE")
+
+print(">>>>>")
+print(f"clean var {SRC_TITLE}")
+print(f"simple var {FOO_VAR}")
+print(f"secret {API_TOKEN}")
+print(">>>>>")
