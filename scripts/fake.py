3efc14f7fba6e6b6e473cd4a85590450937fa568
Tyler Burton <tyler.burton@gsa.gov>
2024-07-19 17:39:39 -0500

    test

--- scripts/fake.py
+++ scripts/fake.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_TOKEN = os.getenv("CKAN_API_TOKEN")
+FAKE_VAR = os.getenv("FAKE_VAR")
 FOO_VAR = os.getenv("FOO_VAR")
 SRC_TITLE = os.getenv("SRC_TITLE")
 
 print(">>>>>")
 print(f"clean var {SRC_TITLE}")
 print(f"simple var {FOO_VAR}")
-print(f"secret {API_TOKEN}")
-print(f"secret is None? {API_TOKEN is None}")
+print(f"secret {FAKE_VAR}")
+print(f"FAKE_VAR equals foo? {FAKE_VAR == 'foo'}")
 print(">>>>>")
