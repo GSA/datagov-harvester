176a342a4300ac80ce8c64e77c128c4c13a9a869
Tyler Burton <tyler.burton@gsa.gov>
2025-01-14 11:25:49 -0600

    instantiate client in preferred way; update gh actions order of operations

--- tests/unit/test_cf_tasks.py
+++ tests/unit/test_cf_tasks.py
@@ -4,18 +4,17 @@ from harvester.lib.cf_handler import CFHandler
 
 
 @patch("harvester.lib.cf_handler.CloudFoundryClient")
-@patch("harvester.lib.cf_handler.TaskManager")
 class TestCFTasking:
-    def test_add_task(self, TMMock, CFClientMock, dhl_cf_task_data):
+    def test_add_task(self, CFClientMock, dhl_cf_task_data):
         CFUtil = CFHandler("url", "user", "password")
         assert CFUtil.start_task(**dhl_cf_task_data) is not None
 
-    def test_get_task(self, TMMock, CFClientMock, dhl_cf_task_data):
+    def test_get_task(self, CFClientMock, dhl_cf_task_data):
         CFUtil = CFHandler("url", "user", "password")
         task = CFUtil.get_task(dhl_cf_task_data["task_id"])
         assert task is not None
 
-    def test_get_all_app_tasks(self, TMMock, CFClientMock, dhl_cf_task_data):
+    def test_get_all_app_tasks(self, CFClientMock, dhl_cf_task_data):
         CFUtil = CFHandler("url", "user", "password")
         # ruff: noqa: E501
         CFClientMock.return_value.v3.apps.__getitem__.return_value.tasks.return_value = [
@@ -24,7 +23,7 @@ class TestCFTasking:
         tasks = CFUtil.get_all_app_tasks(dhl_cf_task_data["app_guuid"])
         assert len(tasks) > 0
 
-    def test_get_all_running_app_tasks(self, TMMock, CFClientMock, dhl_cf_task_data):
+    def test_get_all_running_app_tasks(self, CFClientMock, dhl_cf_task_data):
         CFUtil = CFHandler("url", "user", "password")
         CFClientMock.return_value.v3.apps.__getitem__.return_value.tasks.return_value = [
             {"state": "RUNNING"},
@@ -33,12 +32,12 @@ class TestCFTasking:
         running_tasks = CFUtil.get_all_running_app_tasks(dhl_cf_task_data["app_guuid"])
         assert running_tasks == 1
 
-    def test_cancel_task(self, TMMock, CFClientMock, dhl_cf_task_data):
+    def test_cancel_task(self, CFClientMock, dhl_cf_task_data):
         CFUtil = CFHandler("url", "user", "password")
         task = CFUtil.stop_task(dhl_cf_task_data["task_id"])
         assert task is not None
 
-    def test_read_recent_task_logs(self, TMMock, CFClientMock, dhl_cf_task_data):
+    def test_read_recent_task_logs(self, CFClientMock, dhl_cf_task_data):
         CFUtil = CFHandler("url", "user", "password")
         logs = CFUtil.read_recent_app_logs(
             dhl_cf_task_data["app_guuid"], dhl_cf_task_data["task_id"]
