285861e9f8006b41ade45f662efc42dfb778780e
jbrown-xentity <jbrown@xentity.com>
2025-03-07 16:21:23 -0700

    Make latest change to database.
    This enables the postgis extension for postgresql, and then creates the locations table.
    A follow up manual process will "feed" the locations data into the table, using the `load_csv.sh` script.

---
+++ migrations/versions/bd24037e625a_locations_table_addition.py
@@ -0,0 +1,37 @@
+"""Locations Table Addition
+
+Revision ID: bd24037e625a
+Revises: 63a1161d69b7
+Create Date: 2025-03-07 22:50:36.784734
+
+"""
+from alembic import op
+from geoalchemy2 import Geometry
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'bd24037e625a'
+down_revision = '63a1161d69b7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # op.execute("CREATE EXTENSION postgis;")
+    op.create_table(
+        "locations",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("name", sa.String),
+        sa.Column("type", sa.String),
+        sa.Column("display_name", sa.String),
+        sa.Column("the_geom", Geometry(geometry_type="MULTIPOLYGON")),
+        sa.Column("type_order", sa.String),
+    )
+    # pass
+
+
+def downgrade():
+    op.drop_table("locations")
+    op.execute("DROP EXTENSION postgis_topology;")
+    op.execute("DROP EXTENSION postgis_tiger_geocoder;")
+    op.execute("DROP EXTENSION postgis;")
