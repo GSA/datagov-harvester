974081e100c28d46ca66583516784ceb9f1f67aa
Jin-Sun-tts <jin.sun@gsa.gov>
2025-05-08 16:02:26 -0400

    add deployment steps

--- create_cloudgov_services.sh
+++ create_cloudgov_services.sh
@@ -4,16 +4,16 @@ set -e
 
 # If an argument was provided, use it as the service name prefix. 
 # Otherwise default to "harvesting-logic".
-app_name=${1:-harvesting-logic}
+app_name=${1:-datagov-harvest}
 
 # Get the current space and trim leading whitespace
 space=$(cf target | grep space | cut -d : -f 2 | xargs)
 
 # Production and staging should use bigger DB instances
 if [ "$space" = "prod" ] || [ "$space" = "staging" ]; then
-    cf service "${app_name}-db"    > /dev/null 2>&1 || cf create-service aws-rds medium-psql-redundant "${app_name}-db" --wait&
+    cf service "${app_name}-db"    > /dev/null 2>&1 || cf create-service aws-rds xlarge-gp-psql "${app_name}-db" --wait&
 else
-    cf service "${app_name}-db"    > /dev/null 2>&1 || cf create-service aws-rds micro-psql "${app_name}-db" --wait&
+    cf service "${app_name}-db"    > /dev/null 2>&1 || cf create-service aws-rds medium-gp-psql "${app_name}-db" --wait&
 fi
 
 # create email service
