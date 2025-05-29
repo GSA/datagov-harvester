dcde6f8a3f827d755c8271b7e5e2db59fc5a87a5
Reid Hewitt <92057378+rshewitt@users.noreply.github.com>
2025-05-29 08:42:05 -0600

    add jsonschema format extension to process uris

--- tests/integration/harvest/test_validate.py
+++ tests/integration/harvest/test_validate.py
@@ -43,6 +43,35 @@ class TestValidateDataset:
 
         assert test_record.valid is True
 
+    def test_invalid_license_uri_dcatus_non_federal(
+        self,
+        interface,
+        organization_data,
+        source_data_dcatus_bad_license_uri,
+        job_data_dcatus_non_federal,
+    ):
+        interface.add_organization(organization_data)
+        interface.add_harvest_source(source_data_dcatus_bad_license_uri)
+        harvest_job = interface.add_harvest_job(job_data_dcatus_non_federal)
+
+        harvest_source = HarvestSource(harvest_job.id)
+        harvest_source.prepare_external_data()
+
+        test_id = (
+            "https://www.arcgis.com/home/item.html?id=99731bb0369848169d98f31ce83fb0e2"
+        )
+        test_record = harvest_source.external_records[test_id]
+
+        with pytest.raises(ValidationException) as e:
+            test_record.validate()
+
+        # omitting the entire message for brevity
+        # see the fixture for more details
+        assert e.value.msg.startswith(
+            "<ValidationError: '\"<p align=\\'center\\'"
+        ) and e.value.msg.endswith("is not valid under any of the given schemas'>")
+        assert test_record.valid is False
+
     def test_valid_transformed_iso(
         self,
         interface,
