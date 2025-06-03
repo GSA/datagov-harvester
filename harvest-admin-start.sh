4f1c1704b6dce36b02e942f65ca0ad48a1f02c72
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2025-06-03 15:14:15 -0500

    Try starting a different gunicorn server

--- harvest-admin-start.sh
+++ harvest-admin-start.sh
@@ -2,4 +2,4 @@
 
 DIR="$(dirname "${BASH_SOURCE[0]}")"
 
-exec newrelic-admin run-program gunicorn "wsgi:application" --config "$DIR/gunicorn.conf.py" -b "0.0.0.0:$PORT" --chdir $DIR --timeout 120 --worker-class gevent --workers 8 --forwarded-allow-ips='*'
+exec newrelic-admin run-program gunicorn "wsgi:application" --config "$DIR/gunicorn.conf.py" -b "0.0.0.0:$PORT" --chdir $DIR --timeout 120 --worker-class gthread --workers 3 --forwarded-allow-ips='*'
