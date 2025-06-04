0fbf31a46031d986ce1f7b32ae75f1e2b2858830
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2025-06-04 16:17:31 -0500

    Fix migrations and always run them on app start

--- harvest-admin-start.sh
+++ harvest-admin-start.sh
@@ -2,4 +2,11 @@
 
 DIR="$(dirname "${BASH_SOURCE[0]}")"
 
-exec newrelic-admin run-program gunicorn "wsgi:application" --config "$DIR/gunicorn.conf.py" -b "0.0.0.0:$PORT" --chdir $DIR --timeout 120 --worker-class gthread --workers 3 --forwarded-allow-ips='*'
+# run migrations if we are the first CloudFoundry instance or
+# if there is no CF_INSTANCE_INDEX environment variable
+if [ "$CF_INSTANCE_INDEX" = "0" -o -z "$CF_INSTANCE_INDEX" ]; then
+    echo Running migrations
+    flask db upgrade 
+fi
+
+exec newrelic-admin run-program gunicorn "wsgi:application" --config "$DIR/gunicorn.conf.py" -b "0.0.0.0:$PORT" --chdir $DIR --timeout 120 --worker-class gthread --workers 3 --forwarded-allow-ips='*' --preload
