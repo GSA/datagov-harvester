2500ef476fbda6146795992f77db6fe7cae31c73
jbrown-xentity <jbrown@xentity.com>
2025-06-06 12:08:29 -0600

    Fix ability to clean db

--- scripts/clean-db.py
+++ scripts/clean-db.py
@@ -1,14 +1,23 @@
 import argparse
 import os
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 sys.path.insert(1, "/".join(os.path.realpath(__file__).split("/")[0:-2]))
 
 from harvester import HarvesterDBInterface  # noqa E402
 
+DATABASE_URI = os.getenv("DATABASE_URI")
+
+# create a scopedsession for our harvest runner
+engine = create_engine(DATABASE_URI)
+session_factory = sessionmaker(bind=engine, autoflush=True)
+session = scoped_session(session_factory)
+
 
 def main(days):
-    interface = HarvesterDBInterface()
+    interface = HarvesterDBInterface(session=session)
     outdated_records = interface.get_all_outdated_records(days)
     for record in outdated_records:
         interface.delete_harvest_record(ID=record.id)  # this cascades to errors
