f49ed1dbd3cd5c961a2e044fa2fa9e619b12383d
jbrown-xentity <jbrown@xentity.com>
2025-06-17 07:30:56 -0600

    Roll back to basic create_engine

--- harvester/__init__.py
+++ harvester/__init__.py
@@ -2,7 +2,7 @@ import logging.config
 import os
 
 from dotenv import load_dotenv
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 from config.logger_config import LOGGING_CONFIG
@@ -15,7 +15,7 @@ logging.config.dictConfig(LOGGING_CONFIG)
 DATABASE_URI = os.getenv("DATABASE_URI")
 
 # create a scopedsession for our harvest runner
-engine = create_async_engine(DATABASE_URI)
+engine = create_engine(DATABASE_URI)
 session_factory = sessionmaker(bind=engine, autoflush=True)
 session = scoped_session(session_factory)
 
