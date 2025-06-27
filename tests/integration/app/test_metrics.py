1936a8b3fe3dbf6fc8a39da56c43f323d6bc7bdc
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2025-06-27 15:05:00 -0500

    Fix and test showing only recent failed jobs.

--- tests/integration/app/test_metrics.py
+++ tests/integration/app/test_metrics.py
@@ -6,15 +6,31 @@ import pytest
 
 
 @pytest.fixture()
-def failed_job_error(interface, organization_data, source_data_dcatus, job_data_new):
+def new_job(interface, organization_data, source_data_dcatus, job_data_new):
     interface.add_organization(organization_data)
     interface.add_harvest_source(source_data_dcatus)
-    job = interface.add_harvest_job(job_data_new)
+    yield interface.add_harvest_job(job_data_new)
+
+@pytest.fixture()
+def failed_job_error(interface, new_job):
+    """An error for a failed job."""
     error = interface.add_harvest_job_error(
         {
             "type": "FailedJobCleanup",
-            "harvest_job_id": job.id,
+            "harvest_job_id": new_job.id,
             "date_created": datetime.now() - timedelta(hours=12),
+        }
+    )
+    yield error
+
+@pytest.fixture()
+def old_failed_job_error(interface, new_job):
+    """An error for a failed job more than 24 hours old."""
+    error = interface.add_harvest_job_error(
+        {
+            "type": "FailedJobCleanup",
+            "harvest_job_id": new_job.id,
+            "date_created": datetime.now() - timedelta(hours=25),
         }
     )
     yield error
@@ -26,3 +42,8 @@ class TestMetricsPage:
         """Test that the failed jobs table has rows."""
         resp = client.get("/metrics/")
         assert f'<a href="/harvest_job/{failed_job_error.harvest_job_id}">' in resp.text
+
+    def test_metrics_old_failed_jobs(self, client, old_failed_job_error):
+        """The failed jobs table doesn't contain old jobs."""
+        resp = client.get("/metrics/")
+        assert "No failed jobs" in resp.text
