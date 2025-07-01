a12756b340dcdf34695d2458e309c83c1749fc8b
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2025-07-01 12:21:10 -0500

    Fix merge bug

--- tests/integration/app/test_metrics.py
+++ tests/integration/app/test_metrics.py
@@ -11,19 +11,20 @@ def failed_job_error(interface, job):
     error = interface.add_harvest_job_error(
         {
             "type": "FailedJobCleanup",
-            "harvest_job_id": new_job.id,
+            "harvest_job_id": job.id,
             "date_created": datetime.now() - timedelta(hours=12),
         }
     )
     yield error
 
+
 @pytest.fixture()
-def old_failed_job_error(interface, new_job):
+def old_failed_job_error(interface, job):
     """An error for a failed job more than 24 hours old."""
     error = interface.add_harvest_job_error(
         {
             "type": "FailedJobCleanup",
-            "harvest_job_id": new_job.id,
+            "harvest_job_id": job.id,
             "date_created": datetime.now() - timedelta(hours=25),
         }
     )
@@ -36,6 +37,11 @@ class TestMetricsPage:
         """Failed jobs table has rows."""
         resp = client.get("/metrics/")
         assert f'<a href="/harvest_job/{failed_job_error.harvest_job_id}">' in resp.text
+
+    def test_metrics_old_failed_jobs(self, client, old_failed_job_error):
+        """The failed jobs table doesn't contain old jobs."""
+        resp = client.get("/metrics/")
+        assert "No failed jobs" in resp.text
 
     def test_metrics_failed_source_name(self, client, job, failed_job_error):
         """Failed jobs table links to source by name."""
