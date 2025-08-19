73e69202b338735007fe2e3423bb1e28f1c0c6f8
jbrown-xentity <jbrown@xentity.com>
2025-08-19 15:32:07 -0600

    Update CKAN API usage for correct headers.
    See library: https://github.com/ckan/ckanapi/blob/master/ckanapi/remoteckan.py#L35
    Requires setting user-agent separate from headers.

--- harvester/utils/ckan_utils.py
+++ harvester/utils/ckan_utils.py
@@ -22,7 +22,7 @@ from harvester.exceptions import (
 if typing.TYPE_CHECKING:
     from harvester.harvest import HarvestSource
 
-from harvester.utils.general_utils import get_datetime, is_number, validate_geojson
+from harvester.utils.general_utils import get_datetime, is_number, validate_geojson, USER_AGENT
 
 # all of these are copy/pasted from ckan core
 # https://github.com/ckan/ckan/blob/master/ckan/lib/munge.py
@@ -60,6 +60,7 @@ class CKANSyncTool:
             os.getenv("CKAN_API_URL"),
             apikey=os.getenv("CKAN_API_TOKEN"),
             session=session,
+            user_agent=USER_AGENT
         )
 
     def sync(self, record):
