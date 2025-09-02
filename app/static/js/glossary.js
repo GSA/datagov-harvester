3b9a76b2d6434708b2907bbd42573be826d761f5
Reid Hewitt <92057378+rshewitt@users.noreply.github.com>
2025-09-02 11:56:43 -0600

    add click-to-define

--- app/static/js/glossary.js
+++ app/static/js/glossary.js
@@ -1,6 +1,29 @@
 var Glossary = require("glossary-panel");
 var terms = require("../data/glossary.json");
 
+var body = document.querySelectorAll(
+  ".harvest-source-config-properties td, .harvest-source-config-summary td, .harvest-job-config-properties td, .job-data .section h2"
+);
+
+if (body) {
+  Object.keys(terms).forEach(function (key) {
+    var term = terms[key].term;
+    var re = new RegExp("(\\b" + term + "\\b)(?![^<]*>|[^<>]*</)", "i");
+
+    for (var i = 0; i < body.length; i++) {
+      var match = re.exec(body[i].innerHTML);
+
+      if (match) {
+        body[i].innerHTML = body[i].innerHTML.replace(
+          re,
+          `<span data-term="${term}">${match[0]}</span>`
+        );
+        break;
+      }
+    }
+  });
+}
+
 function decorator(glossary) {
   var accordion = glossary.accordion;
 
