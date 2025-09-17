b33d8906fb687bb9bd92f7c2efca96f66fca6eed
Christopher Hedrick <christopher.hedrick@reisystems.com>
2025-09-17 12:37:08 -0400

    for the harvest-source-config-properties, we make it so we only select the first `td` of the `tr`

--- app/static/js/glossary.js
+++ app/static/js/glossary.js
@@ -2,9 +2,8 @@ var Glossary = require("glossary-panel");
 var terms = require("../data/glossary.json");
 
 var body = document.querySelectorAll(
-  ".harvest-source-config-properties td, .harvest-source-config-summary td, .harvest-job-config-properties td, .job-data .section h2"
+  ".harvest-source-config-properties tr td:first-child, .harvest-source-config-summary td, .harvest-job-config-properties td, .job-data .section h2"
 );
-
 if (body) {
   Object.keys(terms).forEach(function (key) {
     var term = terms[key].term;
@@ -12,7 +11,6 @@ if (body) {
 
     for (var i = 0; i < body.length; i++) {
       var match = re.exec(body[i].innerHTML);
-
       if (match) {
         body[i].innerHTML = body[i].innerHTML.replace(
           re,
@@ -23,7 +21,6 @@ if (body) {
     }
   });
 }
-
 function decorator(glossary) {
   var accordion = glossary.accordion;
 
