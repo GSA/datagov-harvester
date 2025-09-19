6f25e85dc29891ed6f64442423c2e6264e5421ec
Reid Hewitt <92057378+rshewitt@users.noreply.github.com>
2025-09-19 14:02:07 -0600

    add util

--- migrations/versions/a6aa1afd27b7_remove_csdgm_from_schema_type.py
+++ migrations/versions/a6aa1afd27b7_remove_csdgm_from_schema_type.py
@@ -7,13 +7,15 @@ Create Date: 2025-09-12 17:06:56.439448
 """
 from alembic import op
 import sqlalchemy as sa
-
+from migrations.utils import get_terminate_processes_sql_cmd
 
 # revision identifiers, used by Alembic.
 revision = 'a6aa1afd27b7'
 down_revision = '1800d355e5b9'
 branch_labels = None
 depends_on = None
+
+terminate_processes_sql = get_terminate_processes_sql_cmd()
 
 # remove "csdgm" from schema_type
 old_options = (
@@ -35,10 +37,7 @@ new_enum = sa.Enum(*new_options, name="schema_type_new")
 
 def upgrade():
 
-    op.execute(
-        "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() "
-        "AND state = 'active' or state = 'idle in transaction'"
-    )
+    op.execute(terminate_processes_sql)
 
     # Create new enum
     new_enum.create(op.get_bind(), checkfirst=False)
@@ -58,10 +57,7 @@ def upgrade():
 
 def downgrade():
 
-    op.execute(
-        "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() "
-        "AND state = 'active' or state = 'idle in transaction'"
-    )
+    op.execute(terminate_processes_sql)
 
     # recreate the old enum
     old_enum.name = "schema_type_old"
