dc8b07c92cba82f615e8c5a77777e065475e787d
Fuhu Xia <fxia@reisystems.com>
2025-10-02 09:53:07 -0400

    one migration do both fields

--- migrations/versions/7a5d0b40bd85_add_description_and_slug_to_organization.py
+++ migrations/versions/7a5d0b40bd85_add_description_and_slug_to_organization.py
@@ -1,8 +1,8 @@
-"""Add slug column to organization
+"""Add description and slug columns to organization
 
-Revision ID: 8f2dbb3e5f6b
-Revises: 7a5d0b40bd85
-Create Date: 2025-10-01 12:30:00.000000
+Revision ID: 7a5d0b40bd85
+Revises: 1800d355e5b9
+Create Date: 2025-10-01 12:00:00.000000
 
 """
 
@@ -11,14 +11,15 @@ from alembic import op
 
 
 # revision identifiers, used by Alembic.
-revision = "8f2dbb3e5f6b"
-down_revision = "7a5d0b40bd85"
+revision = "7a5d0b40bd85"
+down_revision = "416319f5e5bb"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     with op.batch_alter_table("organization", schema=None) as batch_op:
+        batch_op.add_column(sa.Column("description", sa.Text(), nullable=True))
         batch_op.add_column(sa.Column("slug", sa.String(length=100), nullable=True))
         batch_op.create_unique_constraint("uq_organization_slug", ["slug"])
 
@@ -27,3 +28,5 @@ def downgrade():
     with op.batch_alter_table("organization", schema=None) as batch_op:
         batch_op.drop_constraint("uq_organization_slug", type_="unique")
         batch_op.drop_column("slug")
+        batch_op.drop_column("description")
+
