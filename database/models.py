de3487c33dc52690ed2d38a46d2a9b02e2acbe17
Fuhu Xia <fxia@reisystems.com>
2025-10-03 09:21:58 -0400

    save transformed data

--- database/models.py
+++ database/models.py
@@ -3,6 +3,7 @@ import uuid
 from flask_sqlalchemy import SQLAlchemy
 from geoalchemy2 import Geometry
 from sqlalchemy import CheckConstraint, Column, Enum, String, func
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import DeclarativeBase, backref
 
 
@@ -169,6 +170,7 @@ class HarvestRecord(db.Model):
     )
     source_hash = db.Column(db.String)
     source_raw = db.Column(db.String)
+    source_transform = db.Column(JSONB)
     date_created = db.Column(
         db.DateTime, index=True, default=func.statement_timestamp()
     )
