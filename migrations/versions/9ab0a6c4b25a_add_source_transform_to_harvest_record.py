fdfe6c5ba5a77865f02bf56a58da6ea8073d263f
Fuhu Xia <fxia@reisystems.com>
2025-10-02 22:06:53 -0400

    save transformed data

---
+++ migrations/versions/9ab0a6c4b25a_add_source_transform_to_harvest_record.py
@@ -0,0 +1,29 @@
+"""Add source_transform to harvest_record
+
+Revision ID: 9ab0a6c4b25a
+Revises: a6aa1afd27b7
+Create Date: 2025-09-22 00:00:00.000000
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = "9ab0a6c4b25a"
+down_revision = "a6aa1afd27b7"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table("harvest_record", schema=None) as batch_op:
+        batch_op.add_column(
+            sa.Column("source_transform", postgresql.JSONB(astext_type=sa.Text()), nullable=True)
+        )
+
+
+def downgrade():
+    with op.batch_alter_table("harvest_record", schema=None) as batch_op:
+        batch_op.drop_column("source_transform")
