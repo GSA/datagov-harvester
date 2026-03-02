6feb4dcf1b684c379cbf5e016948d047cd2299e5
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2026-03-02 11:27:10 -0600

    Add test for swagger docs

--- tests/integration/app/test_openapi.py
+++ tests/integration/app/test_openapi.py
@@ -1,5 +1,7 @@
-
 """OpenAPI spec tests."""
+
+from bs4 import BeautifulSoup
+
 
 class TestOpenAPI:
 
@@ -16,3 +18,13 @@ class TestOpenAPI:
         assert "tags" in spec
 
         assert spec["info"]["title"] == "Datagov Harvester"
+
+    def test_openapi_swagger(self, client):
+        response = client.get("/openapi/docs")
+        assert "OpenAPI Documentation" in response.text
+        soup = BeautifulSoup(response.text, "html.parser")
+        assert soup.find("div", id="swagger-ui") is not None
+        assert any(
+            "swagger-ui-bundle.js" in script_el.get("src", "")
+            for script_el in soup.find_all("script")
+        )
