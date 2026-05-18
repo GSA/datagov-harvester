536166380734665d5197b8b2e0d34dec64ffc5a6
Reid Hewitt <92057378+rshewitt@users.noreply.github.com>
2026-05-18 16:34:26 -0400

    add missing/outdated datasets script & action

---
+++ scripts/count_missing_outdated_datasets.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+sys.path.insert(1, "/".join(os.path.realpath(__file__).split("/")[0:-2]))
+
+from harvester import HarvesterDBInterface  # noqa E402
+
+DATABASE_URI = os.getenv("DATABASE_URI")
+
+engine = create_engine(DATABASE_URI)
+session_factory = sessionmaker(bind=engine, autoflush=True)
+session = scoped_session(session_factory)
+
+
+def main():
+    interface = HarvesterDBInterface(session=session)
+    missing_outdated_records = interface.get_missing_or_outdated_datasets()
+    print(missing_outdated_records)  # captured by github action via stdout
+
+
+if __name__ == "__main__":
+    main()
