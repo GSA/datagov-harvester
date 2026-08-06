0596a39bf7ca441b941bbcc03f73e56923a5bf33
Fuhu Xia <fxia@reisystems.com>
2026-08-06 12:09:19 -0400

    get changes into this repo

--- database/models.py
+++ database/models.py
@@ -180,9 +180,9 @@ class HarvestJob(Base):
     records_updated = Column(Integer, default=0)
     records_deleted = Column(Integer, default=0)
     records_errored = Column(Integer, default=0)
+    records_warned = Column(Integer, default=0)
     records_ignored = Column(Integer, default=0)
     records_validated = Column(Integer, default=0)
-    records_warned = Column(Integer, default=0)
 
     errors = relationship(
         "HarvestJobError",
