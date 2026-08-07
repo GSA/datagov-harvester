1def28472253e4f35d6d7dd0bf3931751ecbb795
David Aguiar <david.aguiar@gsa.gov>
2026-08-07 15:17:33 -0600

    Rebase dcatus_catalog migration onto latest main head

--- migrations/versions/f8d4c5ec0e5b_add_dcatus_catalog_to_harvest_job.py
+++ migrations/versions/f8d4c5ec0e5b_add_dcatus_catalog_to_harvest_job.py
@@ -1,7 +1,7 @@
 """Add dcatus_catalog to harvest_job
 
 Revision ID: f8d4c5ec0e5b
-Revises: b3f4a9c1d7e2
+Revises: b3f7a91c24de
 Create Date: 2026-08-05 00:00:00.000000
 
 """
@@ -12,7 +12,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = "f8d4c5ec0e5b"
-down_revision = "b3f4a9c1d7e2"
+down_revision = "b3f7a91c24de"
 branch_labels = None
 depends_on = None
 
