81da89193d0c1b4084c371c96dc064e0768b76f4
Cody Seibert <cody.seibert@gsa.gov>
2026-08-21 16:55:44 -0400

    Rebase drop-orphaned-table migration onto main's new head
    main picked up d4f6a8c0e2b3 (add_type_to_dataset) while this branch was
    open, which revised from the same parent as this migration and left two
    heads. Re-point down_revision at the new head to restore a single chain.

--- migrations/versions/e398f022fd6e_drop_orphaned_harvest_task_control.py
+++ migrations/versions/e398f022fd6e_drop_orphaned_harvest_task_control.py
@@ -1,7 +1,7 @@
 """drop orphaned harvest_task_control table
 
 Revision ID: e398f022fd6e
-Revises: c3e5a7b9d1f3
+Revises: d4f6a8c0e2b3
 Create Date: 2026-08-21 00:00:00.000001
 
 """
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "e398f022fd6e"
-down_revision = "c3e5a7b9d1f3"
+down_revision = "d4f6a8c0e2b3"
 branch_labels = None
 depends_on = None
 
